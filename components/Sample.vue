c300ffa5596a1e422a24253f81e5232ed0d2d053
shuuuu10-01 <tanaka.shu642@mail.kyutech.jp>
2021-05-23 22:57:39 +0900

    [add] nuxt-property-decoratorの導入とそれに伴う変更

---
+++ components/Sample.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>
+      {{ sample }}
+    </h1>
+    nuxt-property-decoratorの使い方は
+    <a href="https://kalappo.net/nuxt-property-decorator-component/">こちら</a>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from 'nuxt-property-decorator'
+
+@Component
+export default class Sample extends Vue {
+  @Prop({ type: String, required: true, default: 'not found' })
+  sample: string|undefined
+}
+</script>
+
+<style scoped>
+</style>
