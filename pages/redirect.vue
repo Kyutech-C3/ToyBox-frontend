6e621df10de0da6f248512119679f59098bf794e
shuuuu10-01 <koron.konan123@gmail.com>
2021-07-28 22:37:55 +0900

    [add] #1　redirect.vueの作成 discordAuthページからリダイレクト後、access_tokenの取得

---
+++ pages/redirect.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    {{ $route.query.code }}
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'nuxt-property-decorator'
+import { AuthStore } from '~/store'
+
+@Component
+export default class Redirect extends Vue {
+  async created () {
+    await AuthStore.fetchUser(String(this.$route.query.code), true)
+  }
+}
+</script>
--- store/auth.ts
+++ store/auth.ts
@@ -3,7 +3,8 @@ import { Module, VuexModule, Mutation, Action } from 'vuex-module-decorators'
 import DiscordOauth2 from 'discord-oauth2'
 const oauthDiscord = new DiscordOauth2({
   clientId: process.env.CLIENT_ID,
-  clientSecret: process.env.CLIENT_SEACRET
+  clientSecret: process.env.CLIENT_SEACRET,
+  redirectUri: process.env.REDIRECT_URL
 })
 
 export interface User {
@@ -46,14 +47,15 @@ export default class Auth extends VuexModule {
     public_flags: 0
   }
 
+  private accessToken: string = ''
+
   public get getTodos () {
     return this.user
   }
 
   private get authURL () {
     return oauthDiscord.generateAuthUrl({
-      scope: ['identify'],
-      redirectUri: process.env.REDIRECT_URL
+      scope: ['identify']
     })
   }
 
@@ -61,9 +63,55 @@ export default class Auth extends VuexModule {
     this.user = user
   }
 
+  @Mutation setAccessToken (token: string) {
+    this.accessToken = token
+  }
+
   @Action({ rawError: true })
-  public authDiscord () {
+  public toAuthDiscordPage () {
     // ここでDiscordログイン実装
     window.location.href = this.authURL
+  }
+
+  @Action({ rawError: true })
+  public fetchUser (token: string, authorization: boolean) {
+    if (authorization) {
+      this.RequestAccessTokenByAuthorizationCode(token)
+    } else {
+      this.RequestAccessTokenByRefleshToken(token)
+    }
+    // const credentials = Buffer.from(
+    //   `${process.env.CLIENT_ID}:${process.env.CLIENT_SEACRET}`
+    // ).toString('base64')
+  }
+
+  @Action({ rawError: true })
+  private RequestAccessTokenByAuthorizationCode (authorizationCode: string) {
+    oauthDiscord
+      .tokenRequest({
+        code: authorizationCode,
+        grantType: 'authorization_code',
+        scope: ['identify']
+      })
+      .then((result) => {
+        this.setAccessToken(result.access_token)
+        localStorage.setItem('refresh_token', result.refresh_token)
+        console.log(result)
+      })
+  }
+
+  @Action({ rawError: true })
+  private RequestAccessTokenByRefleshToken (refreshtoken: string) {
+    oauthDiscord
+      .tokenRequest({
+        refreshToken: refreshtoken,
+        grantType: 'refresh_token',
+        scope: ['identify']
+      })
+      .then((result) => {
+        this.setAccessToken(result.access_token)
+        localStorage.setItem('refresh_token', result.refresh_token)
+        console.log(result)
+      })
   }
 }
