85e687c343f383dcc1ccc423b7afaa7e57760823
shuuuu10-01 <koron.konan123@gmail.com>
2021-07-30 15:28:27 +0900

    [add] #1 /loginページの作成
    見た目の部分は仮置きなので今後デザインが出来たら手を加えてください

---
+++ pages/login.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <button @click="login()">
+      Discordでログイン
+    </button>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'nuxt-property-decorator'
+import { AuthStore } from '~/store'
+
+@Component
+export default class Login extends Vue {
+  login () {
+    AuthStore.toAuthDiscordPage()
+  }
+}
+</script>
