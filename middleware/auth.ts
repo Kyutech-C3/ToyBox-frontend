8798162c31a50e1f628f920a374e79dbf4da1361
shuuuu10-01 <koron.konan123@gmail.com>
2021-09-15 16:29:47 +0900

    [fix] #3 middlewareのif文の修正

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -1,10 +1,9 @@
 import { Middleware } from '@nuxt/types'
 import { AuthStore } from '~/store'
 
-const auth: Middleware = () => {
-  const refreshToken = String(localStorage.getItem('refresh_token'))
-  if (!refreshToken ||
-    AuthStore.getUser.id !== '') {
+const auth: Middleware = ({ route }) => {
+  const refreshToken = localStorage.getItem('refresh_token')
+  if (!refreshToken || AuthStore.nowLogin || route.query.access_token) {
     return
   }
   AuthStore.authAgain()
