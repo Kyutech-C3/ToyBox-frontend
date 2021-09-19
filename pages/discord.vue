b4c40aa884bb3e5a0f778c74df216cbbbdee3dfc
shuuuu10-01 <koron.konan123@gmail.com>
2021-09-19 23:44:03 +0900

    [change] #3 ログイン後のリダイレクトページ('/discord')から自動的に'/'へページ遷移するように変更

--- pages/discord.vue
+++ pages/discord.vue
@@ -1,29 +1,22 @@
 <template>
   <div>
     リダイレクトページ
-    <user-info />
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'nuxt-property-decorator'
 import { AuthStore } from '~/store'
-import UserInfo from '~/components/UserInfo.vue'
-
-@Component({
-  components: {
-    UserInfo
-  }
-})
 
 @Component
 export default class Discord extends Vue {
   created (): void {
-    if (!this.$route.query.access_token) { return }
-
-    AuthStore.newLoginSetAccessToken(String(this.$route.query.access_token))
-    AuthStore.fetchUser(String(this.$route.query.access_token))
-    localStorage.setItem('refresh_token', String(this.$route.query.refresh_token))
+    if (this.$route.query.access_token && this.$route.query.refresh_token) {
+      AuthStore.newLoginSetAccessToken(String(this.$route.query.access_token))
+      AuthStore.fetchUser(String(this.$route.query.access_token))
+      localStorage.setItem('refresh_token', String(this.$route.query.refresh_token))
+    }
+    this.$router.push('/')
   }
 }
 </script>
