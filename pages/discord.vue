7be5db29fbd88688a50efb2cd747417e800ccd25
shuuuu10-01 <koron.konan123@gmail.com>
2021-09-20 00:31:19 +0900

    [update] #3 asyncDataを使ったリダイレクトページの改修

--- pages/discord.vue
+++ pages/discord.vue
@@ -8,14 +8,18 @@
 import { Component, Vue } from 'nuxt-property-decorator'
 import { AuthStore } from '~/store'
 
-@Component
-export default class Discord extends Vue {
-  created (): void {
-    if (this.$route.query.access_token && this.$route.query.refresh_token) {
-      AuthStore.newLoginSetAccessToken(String(this.$route.query.access_token))
-      AuthStore.fetchUser(String(this.$route.query.access_token))
-      localStorage.setItem('refresh_token', String(this.$route.query.refresh_token))
+@Component({
+  components: {},
+  asyncData ({ query }) {
+    if (query.access_token && query.refresh_token) {
+      AuthStore.newLoginSetAccessToken(String(query.access_token))
+      AuthStore.fetchUser(String(query.access_token))
+      localStorage.setItem('refresh_token', String(query.refresh_token))
     }
+  }
+})
+export default class Discord extends Vue {
+  mounted (): void {
     this.$router.push('/')
   }
 }
