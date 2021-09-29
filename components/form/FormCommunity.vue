6d4a25b8efc88bf8169c75771e327a3456e81e16
shuuuu10-01 <koron.konan123@gmail.com>
2021-09-29 21:41:54 +0900

    [update] #12 コミュニティのリストをバックエンドから受け取るように改修

--- components/form/FormCommunity.vue
+++ components/form/FormCommunity.vue
@@ -1,5 +1,10 @@
 <template>
-  <select v-model="communityId" name="pets" required class="w-full p-3 ring-1 focus:outline-none focus:ring-blue-600">
+  <select
+    v-if="communityList.length > 0"
+    v-model="communityId"
+    required
+    class="w-full p-3 ring-1 focus:outline-none focus:ring-blue-600"
+  >
     <option value="" hidden disabled selected>
       community
     </option>
@@ -13,29 +18,29 @@
 
 <script lang="ts">
 import { Component, Vue, VModel } from 'nuxt-property-decorator'
+import axios from 'axios'
+axios.defaults.baseURL = process.env.SERVER_URL
 
 @Component
 export default class FormCommunity extends Vue {
   @VModel({ type: String })
   communityId!: string
 
-  communityList = [
-    {
-      id: 'aaa',
-      name: 'community1'
-    },
-    {
-      id: 'bbb',
-      name: 'community2'
-    },
-    {
-      id: 'ccc',
-      name: 'community3'
-    }
-  ]
+  communityList = []
 
   created () {
-    // ここでコミュニティーのリストを取得する↑communityListは仮置き
+    axios.get('/communities')
+      .then((result) => {
+        if (result.data.length !== 0) {
+          this.communityList = result.data
+          console.log(this.communityList)
+        } else {
+          alert('コミュニティーが登録されていません。')
+        }
+      })
+      .catch((error) => {
+        alert(error)
+      })
   }
 }
 </script>
