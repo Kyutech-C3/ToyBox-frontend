0206420b3d6a80afd15d248f4d1c386581598a1e
shuuuu10-01 <koron.konan123@gmail.com>
2021-09-29 16:46:57 +0900

    [add] #12 formの各コンポーネントを作成

---
+++ components/form/FormTitle.vue
@@ -0,0 +1,13 @@
+<template>
+  <input v-model="title" type="text" name="text/title" placeholder="title" required>
+</template>
+
+<script lang="ts">
+import { Component, Vue, VModel } from 'nuxt-property-decorator'
+
+@Component
+export default class FormTitle extends Vue {
+  @VModel({ type: String })
+  title!: string
+}
+</script>
