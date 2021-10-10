86e0637e995e14544324f22c7cd08979c2c83a27
shuuuu10-01 <koron.konan123@gmail.com>
2021-10-10 18:04:08 +0900

    [add] #12 markdownの追加

--- nuxt.config.js
+++ nuxt.config.js
@@ -17,11 +17,13 @@ export default {
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
-    '~/assets/css/tailwind.css'
+    '~/assets/css/tailwind.css',
+    '~/assets/css/markdown.css'
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
+    { src: '~plugins/nuxt-simplemde.ts', mode: 'client' }
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
