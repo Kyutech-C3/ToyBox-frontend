9cab4c617f3a764c46c5fb270ca253f004bf3efa
shuuuu10-01 <koron.konan123@gmail.com>
2021-10-10 18:04:08 +0900

    [add] #12 markdownの追加

--- nuxt.config.js
+++ nuxt.config.js
@@ -17,12 +17,14 @@ export default {
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
-    '~/assets/css/tailwind.css'
+    '~/assets/css/tailwind.css',
+    '~/assets/css/markdown.css'
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
-    '~/plugins/fontawesome.ts'
+    '~/plugins/fontawesome.ts',
+    { src: '~plugins/nuxt-simplemde.ts', mode: 'client' }
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
---
+++ storybooks/FormMarkdownEditor.stories.ts
@@ -0,0 +1,15 @@
+import FormMarkdown from '~/components/form/FormMarkdown.vue'
+
+export default {
+  title: 'Form'
+}
+
+export const markdown1 = () => ({
+  template: '<form-markdown v-model="description" />',
+  components: { FormMarkdown },
+  data () {
+    return {
+      description: '# sample text'
+    }
+  }
+})
