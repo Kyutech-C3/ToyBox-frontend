afbf6f85efc6ca10815dae788842b69d6c0de0b5
rkun123 <tren111213@gmail.com>
2021-10-18 01:12:49 +0900

    [update] ToyboxButton.vue to use tailwindcss

--- components/ToyboxButton.vue
+++ components/ToyboxButton.vue
@@ -1,6 +1,6 @@
 <template>
   <button
-    class="button"
+    class="rounded-lg border-2 border-yellow-500 text-yellow-500 hover:bg-yellow-500 hover:text-gray-100 transition-colors px-4 py-2.5"
     :class="{shadow: isShadow}"
     @click="(event) => ($emit('click', event))"
   >
@@ -20,39 +20,3 @@ export default class ToyboxButton extends Vue {
   isShadow!: boolean
 }
 </script>
-
-<style lang="scss" scoped>
-$button-min-width: 80px;
-$button-border: 1px;
-$button-font-size-base: 1.2rem;
-
-.button {
-  font-size: $button-font-size-base;
-  display: flex;
-  justify-content: center;
-  min-width: $button-min-width;
-  flex-direction: row;
-  cursor: pointer;
-  padding: 0.6rem 1rem;
-  border-radius: $base-border-radius;
-  border: solid $button-border $accent-color;
-  color: $accent-color;
-  background-color: transparent;
-  font-weight: $font-weight-base;
-
-  transition: background-color 0.1s linear;
-
-  &:hover {
-    background-color: $accent-color;
-    color: $white;
-  }
-}
-
-.shadow {
-  box-shadow:
-    $base-shadow-offset-y
-    $base-shadow-offset-y
-    $base-shadow-blur
-    $base-shadow-color;
-}
-</style>
