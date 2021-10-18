a58d0249cfd42b068ca23572381bbeb465a02d41
shuuuu10-01 <koron.konan123@gmail.com>
2021-10-18 18:03:06 +0900

    [update] #12 タイトル入力部分のCSSを改善

--- components/form/FormTitle.vue
+++ components/form/FormTitle.vue
@@ -1,18 +1,26 @@
 <template>
-  <input
-    v-model="title"
-    type="text"
-    name="text/title"
-    placeholder="title"
-    required
-    class="w-full p-3 ring-1 placeholder-gray-400 focus:outline-none focus:ring-blue-600"
-  >
+  <div class="flex items-center">
+    <form-label name="タイトル" />
+    <input
+      v-model="title"
+      type="text"
+      name="text/title"
+      placeholder="title"
+      required
+      class="w-72 text-xl p-3 border-b-2 placeholder-gray-400 focus:outline-none"
+    >
+  </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue, VModel } from 'nuxt-property-decorator'
+import FormLabel from '~/components/form/FormLabel.vue'
 
-@Component
+@Component({
+  components: {
+    FormLabel
+  }
+})
 export default class FormTitle extends Vue {
   @VModel({ type: String })
   title!: string
