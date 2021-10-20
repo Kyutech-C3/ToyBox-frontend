cb5ddb196f5fe8d4a8cef25469e0b0d5f52585dc
shuuuu10-01 <koron.konan123@gmail.com>
2021-10-20 12:22:02 +0900

    [fix] #12 無駄なタグがあったのでリファクタリング

--- components/form/FormSubmitButton.vue
+++ components/form/FormSubmitButton.vue
@@ -19,33 +19,29 @@
     <transition name="fade">
       <div
         v-if="openList"
-        class="balloon absolute bottom-full rounded-lg bg-white px-5 py-2 border-2 border-yellow-500"
+        class="balloon absolute bottom-16 rounded-lg bg-white px-5 py-2 border-2 border-yellow-500"
       >
         <ul class="z-10 relative">
-          <li v-for="(range, i) in submitList" :key="i">
-            <label :for="range.id" class="flex items-center py-2">
-              <font-awesome-icon :icon="['fas', 'check']" class="w-4 mr-3" :class="{'text-transparent': option != range.value}" />
-              <font-awesome-icon :icon="range.icon" class="w-4 mr-3" />
-              {{ range.name }}
-              <input
-                :id="range.id"
-                v-model="option"
-                :value="range.value"
-                type="radio"
-                class="hidden"
-              >
-            </label>
+          <li
+            v-for="(range, i) in submitList"
+            :key="i"
+            class="cursor-pointer flex items-center py-2"
+            @click="clickOption(i)"
+          >
+            <font-awesome-icon :icon="['fas', 'check']" class="w-4 mr-3" :class="{'text-transparent': option != range.value}" />
+            <font-awesome-icon :icon="range.icon" class="w-4 mr-3" />
+            {{ range.name }}
           </li>
         </ul>
         <!-- 以下、別部分をクリックした時にfalseにするdiv -->
-        <span class="fixed w-full h-full top-0 left-0 z-0" @click="openList = !openList" />
+        <div class="fixed w-full h-full top-0 left-0 z-0" @click="openList = !openList" />
       </div>
     </transition>
   </div>
 </template>
 
 <script lang="ts">
-import { Vue, Component, Watch } from 'nuxt-property-decorator'
+import { Vue, Component } from 'nuxt-property-decorator'
 
 @Component
 export default class FormSubmitButton extends Vue {
@@ -55,26 +51,23 @@ export default class FormSubmitButton extends Vue {
     {
       icon: ['fas', 'upload'],
       name: '公開',
-      value: 0,
-      id: 'release'
+      value: 0
     },
     {
       icon: ['fas', 'lock'],
       name: '限定公開',
-      value: 1,
-      id: 'exclusive'
+      value: 1
     },
     {
       icon: ['far', 'save'],
       name: '下書き保存',
-      value: 2,
-      id: 'save'
+      value: 2
     }
   ]
 
-  @Watch('option')
-  onOptionChanged () {
+  clickOption (number: number) {
     this.openList = !this.openList
+    this.option = number
   }
 }
 </script>
@@ -85,7 +78,7 @@ export default class FormSubmitButton extends Vue {
   transition: opacity .2s;
 }
 .fade-leave-active {
-  transition: opacity .1s;
+  transition: opacity .0s;
 }
 .fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */
 {
@@ -102,7 +95,7 @@ export default class FormSubmitButton extends Vue {
   height: 22px;
 }
 .balloon:before{
-  bottom: -21px;
+  bottom: -20.5px;
   border-top:11px solid #ffffff;
   border-right:10px solid transparent;
   border-left:10px solid transparent;
@@ -111,7 +104,7 @@ export default class FormSubmitButton extends Vue {
   z-index: 10;
 }
 .balloon:after{
-  bottom: -23px;
+  bottom: -22.5px;
   border-top:11px solid #f59e0b;
   border-right:10px solid transparent;
   border-left:10px solid transparent;
