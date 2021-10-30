3ba0ee764cf35c45d2bf76e2ac24c6f5d291d364
citrus-candy <kyuuri11013@gmail.com>
2021-10-30 22:27:20 +0900

    [add] #15 要素のコンポーネント化とstorybookの対応

---
+++ components/Users/Profile.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="flex flex-col content-center justify-center items-center">
+    <div class="m-5">
+      <img class="w-64 border-2 border-black rounded-full" :src="userImage" alt="User image">
+    </div>
+    <div class="text-4xl m-5">
+      {{ userName }}
+    </div>
+    <div class="text-2xl m-5">
+      {{ userDescription }}
+    </div>
+    <div class="m-5">
+      <button
+        class="bg-green-400 hover:bg-green-500 px-5 py-3 rounded-full shadow-md"
+        @click="showEditProfile = true"
+      >
+        プロフィール編集
+      </button>
+      <div
+        v-show="showEditProfile"
+        class="bg-gray-300 fixed h-screen inset-0 bg-opacity-50 flex justify-center items-center"
+      >
+        <div class="bg-white w-2/3 h-2/3 rounded-md">
+          <button
+            class="bg-green-400 hover:bg-green-500 px-5 py-3 rounded-full shadow-md"
+            @click="showEditProfile = false"
+          >
+            とじる
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from 'nuxt-property-decorator'
+
+@Component
+export default class Profile extends Vue {
+  @Prop({ type: String, required: true })
+  userImage!: string
+
+  @Prop({ type: String, required: true })
+  userName!: string
+
+  @Prop({ type: String, required: true })
+  userDescription!: string
+
+  showEditProfile: boolean = false
+}
+</script>
--- pages/users.vue
+++ pages/users.vue
@@ -1,58 +1,30 @@
 <template>
   <div class="min-h-screen flex flex-col content-center justify-center items-center">
-    <div class="m-5">
-      <img class="w-64 border-2 border-black rounded-full" :src="userImage" alt="User image">
-    </div>
-    <div class="text-4xl m-5">
-      {{ userName }}
-    </div>
-    <div class="text-2xl m-5">
-      {{ userDescription }}
-    </div>
-    <div class="m-5">
-      <button
-        class="bg-green-400 hover:bg-green-500 px-5 py-3 rounded-full shadow-md"
-        @click="showEditProfile = true"
-      >
-        プロフィール編集
-      </button>
-      <div
-        v-show="showEditProfile"
-        class="bg-gray-300 fixed h-screen inset-0 bg-opacity-50 flex justify-center items-center"
-      >
-        <div class="bg-white w-2/3 h-2/3 rounded-md">
-          <button
-            class="bg-green-400 hover:bg-green-500 px-5 py-3 rounded-full shadow-md"
-            @click="showEditProfile = false"
-          >
-            とじる
-          </button>
-        </div>
-      </div>
-    </div>
-    <div class="m-10">
-      <div class="text-3xl text-center p-10 border-b-2">
-        作品一覧
-      </div>
-      <div class="m-5 flex flex-wrap justify-center max-w-6xl">
-        <div v-for="(userWork, i) in userWorks" :key="i" class="bg-gray-400 w-80 h-80 m-5 rounded-3xl">
-          {{ userWork }}
-        </div>
-      </div>
-    </div>
+    <profile :user-image="userImage" :user-name="userName" :user-description="userDescription" />
+    <works-filter />
+    <works :user-works="userWorks" />
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'nuxt-property-decorator'
+import Profile from '@/components/Users/Profile.vue'
+import WorksFilter from '~/components/Users/WorksFilter.vue'
+import Works from '~/components/Users/Works.vue'
 
-@Component
+@Component({
+  components: {
+    Profile,
+    WorksFilter,
+    Works
+  }
+})
 export default class Users extends Vue {
   userImage: string = 'http://3.bp.blogspot.com/-n0PpkJL1BxE/VCIitXhWwpI/AAAAAAAAmfE/xLraJLXXrgk/s800/animal_hamster.png'
   userName: string = 'ハムタロサァン'
   userDescription: string = 'はむたろなのだ'
   userWorksCount: number = 6
-  userWorks: Array<string> = Array(this.userWorksCount)
+  userWorks: string[] = Array(this.userWorksCount)
   showEditProfile: boolean = false
 }
 </script>
