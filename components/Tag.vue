435a2963b6822aafed386eb0a0da925b83c431f8
Simo-C3 <shimomae.hitoshi210@mail.kyutech.jp>
2021-11-14 00:25:09 +0900

    [add] #26 tagコンポーネントの追加

---
+++ components/Tag.vue
@@ -0,0 +1,18 @@
+<template>
+  <dev class="bg-white border-black border-solid border-2 text-black px-4 rounded-full" :class="[[fontSize=='xs' || fontSize=='sm' ? 'py-0.5' : 'py-1'], {'text-xs': fontSize=='xs', 'text-sm': fontSize=='sm', 'text-base': fontSize=='base', 'text-lg': fontSize=='lg', 'text-xl': fontSize=='xl'}]">
+    {{ tagText }}
+  </dev>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator'
+
+@Component
+export default class Tag extends Vue {
+  @Prop({ type: String, required: true, default: 'empty' })
+  tagText!: string
+
+  @Prop({ type: String, required: true, default: 'base' })
+  fontSize!: string
+}
+</script>
