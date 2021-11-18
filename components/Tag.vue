c7d3e21b79c967d6cdffa4b0c8948eb3bb4541d6
Simo-C3 <shimomae.hitoshi210@mail.kyutech.jp>
2021-11-19 02:03:52 +0900

    [change] タグから検索URLに飛べるように機能追加

--- components/Tag.vue
+++ components/Tag.vue
@@ -1,8 +1,11 @@
 <template>
-  <div class="bg-white text-black px-4 rounded-lg border border-black w-auto inline-block"
-    :class="[[fontSize=='xs' || fontSize=='sm' ? 'py-0.5' : 'py-1'], {'text-xs': fontSize=='xs', 'text-sm': fontSize=='sm', 'text-base': fontSize=='base', 'text-lg': fontSize=='lg', 'text-xl': fontSize=='xl'}]">
-    {{ tagText }}
-  </div>
+  <nuxt-link :to="'/search?tagId=' + tagId" :class="{ 'is-disabled': linkDisabledFlag }">
+    <div class="bg-white text-black px-4 rounded-lg border border-black w-auto inline-block"
+      :class="[[fontSize=='xs' || fontSize=='sm' ? 'py-0.5' : 'py-1'], {'text-xs': fontSize=='xs', 'text-sm': fontSize=='sm', 'text-base': fontSize=='base', 'text-lg': fontSize=='lg', 'text-xl': fontSize=='xl'}]"
+    >
+      {{ tagText }}
+    </div>
+  </nuxt-link>
 </template>
 
 <script lang="ts">
@@ -13,10 +16,19 @@ export default class Tag extends Vue {
   @Prop({ type: String, required: true, default: '' })
   tagText!: string
 
+  @Prop({ type: String, required: false, default: 'hoge' })
+  tagId!: string
+
+  @Prop({ type: Boolean, required: false, default: 'true' })
+  linkDisabledFlag!: boolean
+
   @Prop({ type: String, required: true, default: '' })
   fontSize!: string
 }
 </script>
 
 <style lang="scss" scoped>
+.is-disabled {
+  pointer-events: none;
+}
 </style>
--- storybooks/Tag.stories.ts
+++ storybooks/Tag.stories.ts
@@ -5,14 +5,20 @@ export default {
 }
 
 export const tag1 = () => ({
-  template: '<tag :tag-text="text" :font-size="size"/>',
+  template: '<tag :tag-text="text" :font-size="size" :tagId="id" :linkDisabledFlag="disable"/>',
   components: { Tag },
   props: {
     text: {
-      default: 'hoge'
+      default: 'hogehoge'
+    },
+    id: {
+      default: '12314'
+    },
+    disable: {
+      default: false
     },
     size: {
-      default: 'base'
+      default: 'xs'
     }
   }
 })
