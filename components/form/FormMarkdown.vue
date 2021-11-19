50284d93dc880ab8fe07d150e0f775212d6865d7
shuuuu10-01 <koron.konan123@gmail.com>
2021-11-19 12:42:46 +0900

    [change] #12 マークダウンエディターの使用ライブラリを変更

--- components/form/FormMarkdown.vue
+++ components/form/FormMarkdown.vue
@@ -1,5 +1,16 @@
 <template>
-  <vue-simplemde v-model="description" class="markdown-body min-h-300 w-full" :configs="configs" />
+  <mavon-editor
+    v-model="description"
+    :toolbars="toolbarsOption"
+    language="ja"
+    xss-options="{}"
+    default-open="edit"
+    class="
+    markdown-body
+    min-h-300
+    w-full
+    "
+  />
 </template>
 
 <script lang="ts">
@@ -10,45 +21,24 @@ export default class FormMarkdown extends Vue {
   @VModel({ type: String })
   description!: string
 
-  configs = {
-    autosave: {
-      enabled: false
-    },
-    placeholder: 'マークダウンでの記入が可能です',
-    spellChecker: false,
-    status: false,
-    initialValue: '',
-    toolbar: [
-      'preview',
-      '|',
-      'bold',
-      'italic',
-      'strikethrough',
-      'heading',
-      '|',
-      'code',
-      'quote',
-      'link',
-      '|',
-      'unordered-list',
-      'ordered-list',
-      'table',
-      'horizontal-rule',
-      '|',
-      'guide'
-    ]
+  toolbarsOption = {
+    bold: true, // 粗体
+    italic: true, // 斜体
+    header: true, // 标题
+    underline: true, // 下划线
+    strikethrough: true, // 中划线
+    quote: true, // 引用
+    ol: true, // 有序列表
+    ul: true, // 无序列表
+    link: true, // 链接
+    code: true, // code
+    table: true, // 表格
+    /* 1.3.5 */
+    undo: true, // 上一步
+    redo: true, // 下一步
+    /* 2.2.1 */
+    subfield: true, // 单双栏模式
+    preview: true // 预览
   }
 }
 </script>
-
-<style>
-.editor-toolbar {
-  border-top: 1px solid rgba(156, 163, 175, var(--tw-border-opacity));
-  border-left: 1px solid rgba(156, 163, 175, var(--tw-border-opacity));
-  border-right: 1px solid rgba(156, 163, 175, var(--tw-border-opacity));
-}
-.CodeMirror {
-  border: 1px solid rgba(156, 163, 175, var(--tw-border-opacity));
-  opacity: 0.8;
-}
-</style>
--- nuxt.config.js
+++ nuxt.config.js
@@ -25,7 +25,7 @@ export default {
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
     '~/plugins/fontawesome.ts',
-    { src: '~plugins/nuxt-simplemde.ts', mode: 'client' }
+    { src: '~plugins/mavonEditor.ts', mode: 'client' }
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
