c8820c8c0d259f19547b9d583ac1858bd52c1c36
citrus-candy <kyuuri11013@gmail.com>
2021-12-12 22:53:09 +0900

    [change] 中身をコンポーネントに分割

---
+++ components/works/WorksTitle.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <p class="font-light text-gray-400">
+      2021/12/24 12:00
+    </p>
+    <p class="text-3xl">
+      ToyBox Frontend
+    </p>
+    <div class="inline-flex items-center mt-5">
+      <img class="border rounded-full w-10 h-10 border-gray-400" src="http://3.bp.blogspot.com/-n0PpkJL1BxE/VCIitXhWwpI/AAAAAAAAmfE/xLraJLXXrgk/s800/animal_hamster.png" alt="">
+      <p class="ml-3">
+        ハムタロサァン
+      </p>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'nuxt-property-decorator'
+
+@Component
+export default class WorksTitle extends Vue {
+
+}
+</script>
--- pages/works/_id.vue
+++ pages/works/_id.vue
@@ -2,60 +2,35 @@
   <div class="w-8/12 m-auto">
     <carousel class="border border-gray-700" />
     <div class="flex justify-between mt-5">
-      <div>
-        <div class="font-light text-gray-400">
-          2021/12/24 12:00
-        </div>
-        <div class="text-3xl">
-          ToyBox Frontend
-        </div>
-        <div class="inline-flex items-center mt-5">
-          <img class="border rounded-full w-10 h-10 border-gray-400" src="http://3.bp.blogspot.com/-n0PpkJL1BxE/VCIitXhWwpI/AAAAAAAAmfE/xLraJLXXrgk/s800/animal_hamster.png" alt="">
-          <div class="ml-3">
-            ハムタロサァン
-          </div>
-        </div>
-      </div>
-      <div class="self-center font-light text-gray-400">
-        <div>
-          1000 view
-        </div>
-        <div class="inline-flex items-center mt-3">
-          <font-awesome-icon class="w-7" :icon="['far', 'heart']" />
-          <div class="text-lg ml-2">
-            10
-          </div>
-        </div>
-      </div>
+      <works-title />
+      <works-view-info />
     </div>
     <!-- コミュニティー -->
-    <div class="flex mt-8">
-      <font-awesome-icon class="w-6 text-gray-500" :icon="['fas', 'users']" />
-      <div class="border border-gray-500 rounded-lg text-center px-6 mx-3">
+    <works-content class="mt-8" icon="users">
+      <p class="border border-gray-500 rounded-lg text-center px-6 mx-3">
         Hack
-      </div>
-    </div>
+      </p>
+    </works-content>
     <!-- タグ -->
-    <div class="flex mt-5">
-      <font-awesome-icon class="w-6 text-gray-500" :icon="['fas', 'tags']" />
-      <div class="border border-gray-500 rounded-lg text-center px-6 mx-3">
+    <works-content class="mt-5" icon="tags">
+      <p class="border border-gray-500 rounded-lg text-center px-6 mx-3">
         Nuxt.js
-      </div>
-      <div class="border border-gray-500 rounded-lg text-center px-6 mx-3">
+      </p>
+      <p class="border border-gray-500 rounded-lg text-center px-6 mx-3">
         TypeScript
-      </div>
-    </div>
+      </p>
+    </works-content>
     <!-- URL -->
-    <div class="flex mt-8">
-      <font-awesome-icon class="w-6 text-gray-500" :icon="['fas', 'link']" />
-      <a class="mx-3 hover:text-blue-600 hover:underline" href="https://google.com" target="_blank">google.com</a>
-    </div>
+    <works-content class="mt-8" icon="link">
+      <a class="mx-3 hover:text-blue-600 hover:underline" href="https://google.com" target="_blank">
+        google.com
+      </a>
+    </works-content>
     <!-- ダウンロード -->
-    <div class="flex mt-5">
-      <font-awesome-icon class="w-6 text-gray-500" :icon="['fas', 'download']" />
+    <works-content class="mt-5" icon="download">
       <custom-button class="mx-3" title="hogefuga.zip" />
       <custom-button class="mx-3" title="foobar.mp3" />
-    </div>
+    </works-content>
     <!-- 説明 -->
     <div class="mt-10">
       Toyboxは、組込みシステム向けのいくつかのUNIXのコマンドラインユーティリティの代替として設計されたフリーでオープンソースのソフトウェアある。<br>
@@ -73,11 +48,15 @@
 <script lang="ts">
 import { Vue, Component } from 'nuxt-property-decorator'
 import Carousel from '@/components/Carousel.vue'
+import WorksTitle from '@/components/works/WorksTitle.vue'
+import WorksViewInfo from '@/components/works/WorksViewInfo.vue'
 import CustomButton from '@/components/ToyboxButton.vue'
 
 @Component({
   components: {
     Carousel,
+    WorksTitle,
+    WorksViewInfo,
     CustomButton
   }
 })
@@ -85,7 +64,3 @@ export default class Works extends Vue {
 
 }
 </script>
-
-<style scoped>
-
-</style>
