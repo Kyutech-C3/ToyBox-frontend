43675ed99add361ca0bd75f835297852253e65a5
kyuuri11013 <kyuuri11013@gmail.com>
2022-02-27 21:55:21 +0900

    add: eslint and prettier settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,12 +4,22 @@ module.exports = {
     browser: true,
     node: true
   },
-  extends: [
-    '@nuxtjs/eslint-config-typescript',
-    'plugin:nuxt/recommended'
-  ],
-  plugins: [
-  ],
+  extends: ['@nuxtjs/eslint-config-typescript', 'plugin:nuxt/recommended'],
+  plugins: ['vue'],
   // add your custom rules here
-  rules: {}
+  rules: {
+    'space-before-function-paren': ['error', 'never'],
+    'comma-spacing': [
+      'error',
+      {
+        before: false,
+        after: true
+      }
+    ],
+    'comma-dangle': ['error', 'never'],
+    semi: ['error', 'never'],
+    quotes: [2, 'single'],
+    'keyword-spacing': ['error'],
+    'vue/html-self-closing': 'off'
+  }
 }
