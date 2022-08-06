a85f2e6e53fd52266cb8988075e6bc3124e378c1
Simo-C3 <shimomae.hitoshi210@mail.kyutech.jp>
2022-08-07 04:06:02 +0900

    [add] infomationモーダルコンポーネントの追加

---
+++ components/commons/Info.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="relative w-min inline-block">
+    <font-awesome-icon
+      class="font-awesome-icon w-4 h-4 inline text-gray-400 hover:text-gray-600"
+      :icon="['fas', 'info-circle']"
+    />
+    <div
+      class="
+        invisible
+        absolute
+        px-5
+        text-xs
+        border
+        rounded-md
+        drop-shadow-md
+        top-2
+        left-6
+        bg-gray-200
+        whitespace-pre-wrap
+        w-max
+        z-50
+        info-modal
+      "
+    >
+      {{ info }}
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'nuxt-property-decorator'
+
+@Component
+export default class Info extends Vue {
+  @Prop({ type: String, required: false })
+  info!: string
+}
+</script>
+
+<style scoped>
+.font-awesome-icon:hover + .info-modal {
+  visibility: visible;
+}
+</style>
