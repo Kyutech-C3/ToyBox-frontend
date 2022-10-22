21f3fefef8262a784ff6ce2000d327ebf55f41ea
Simo-C3 <shimomae.hitoshi210@mail.kyutech.jp>
2022-10-23 00:31:59 +0900

    [add] meta data

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,14 +1,55 @@
+import axios from 'axios'
+
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'c3-toybox',
+    title: 'ToyBox',
     htmlAttrs: {
-      lang: 'ja'
+      lang: 'ja',
+      prefix:
+        'og: https://ogp.me/ns# fb: https://ogp.me/ns/fb# website: https://ogp.me/ns/website#'
     },
     meta: [
       { charset: 'utf-8' },
+      {
+        name: 'copyright',
+        content:
+          '© 2022' + new Date().getFullYear() + ' Composite Computer Club'
+      },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+      {
+        hid: 'description',
+        name: 'description',
+        content:
+          'ToyBoxは九州工業大学情報工学部の Composite Computer Club（C3）の作品ポータルサイトです。C3部員が製作した作品の閲覧が出来ます。'
+      },
+      { name: 'format-detection', content: 'telephone=no' },
+      { 'http-equiv': 'Cache-Control', content: 'no-cache' },
+      { name: 'twitter:card', content: 'summary_large_image' },
+      { name: 'twitter:site', content: '@c3_kyutech' },
+      { hid: 'og:url', property: 'og:url', content: process.env.BASE_URL },
+      {
+        hid: 'og:site_name',
+        property: 'og:site_name',
+        content: 'ToyBox'
+      },
+      {
+        hid: 'og:title',
+        property: 'og:title',
+        content: 'ToyBox - ホーム'
+      },
+      { hid: 'og:type', property: 'og:type', content: 'website' },
+      {
+        hid: 'og:description',
+        property: 'og:description',
+        content:
+          'ToyBoxは九州工業大学情報工学部の Composite Computer Club（C3）の作品ポータルサイトです。C3部員が製作した作品の閲覧が出来ます。'
+      },
+      {
+        hid: 'og:image',
+        property: 'og:image',
+        content: 'https://simo-c3.github.io/image_url/c3_logo_circled.png' // ロゴができたら置き換え
+      }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
@@ -98,11 +139,26 @@ export default {
   },
   ssr: false,
 
-  router: {},
+  router: { base: '/' },
 
   watchers: {
     webpack: {
       ignored: /.git/
+    }
+  },
+
+  generate: {
+    fallback: true,
+    async routes() {
+      return Promise.all([
+        await axios.get(process.env.API_URL + '/users?limit=9999'),
+        await axios.get(process.env.API_URL + '/works?limit=9999')
+      ]).then(([users, works]) => {
+        const urls = []
+        users.data.map((user) => urls.push({ route: `/users/${user.id}` }))
+        works.data.map((work) => urls.push({ route: `/works/${work.id}` }))
+        return urls
+      })
     }
   }
 }
