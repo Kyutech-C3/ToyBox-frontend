497309bce1d4eb49537b9b15b60dd366f723147b
Simo-C3 <shimomae.hitoshi210@mail.kyutech.jp>
2023-12-11 14:44:53 +0900

    [change] デザインの微修正

--- components/blogs/form/FormAssetList.vue
+++ components/blogs/form/FormAssetList.vue
@@ -2,15 +2,16 @@
   <div>
     <div
       class="
-        bg-hover-base-button-color
+        bg-gray-100
         py-1
+        px-4
         mb-4
         w-full
         rounded-md
         overflow-x-auto overflow-y-clip
       "
     >
-      <div class="px-4 flex items-center gap-2 h-40">
+      <div class="flex items-center gap-2 h-40">
         <div
           v-for="asset in assets"
           class="
@@ -21,19 +22,20 @@
             justify-center
             items-center
             relative
-            bg-gray-700
-            rounded-sm
+            bg-white
+            rounded-md
+            overflow-hidden
           "
         >
           <img
             :src="asset.url"
-            class="w-36 h-36"
+            class="object-contain w-full h-full"
             v-if="asset.asset_type === 'image'"
           />
           <video
             width="144px"
             height="144px"
-            class="w-36 h-36"
+            class="object-contain w-full h-full"
             webkit-playsinline
             playsinline
             muted
@@ -44,15 +46,24 @@
             <source :src="asset.url" type="video/mp4" />
             <source :src="asset.url" type="video/quicktime" />
           </video>
-          <div class="z-50 absolute right-2 top-2">
-            <base-icon-button
-              :size="'small'"
-              :font-awesome="{
-                type: 'fas',
-                name: 'up-right-and-down-left-from-center'
-              }"
+          <div
+            class="
+              z-50
+              absolute
+              right-2
+              bottom-2
+              flex
+              items-center
+              gap-2
+              select-none
+            "
+          >
+            <button
               @click="() => insertAssetUrl(asset.id)"
-            />
+              class="text-xs rounded-md py-1 px-3 text-white bg-slate-800/40"
+            >
+              挿入
+            </button>
           </div>
         </div>
       </div>
